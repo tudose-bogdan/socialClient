c02818be17a1e0c021a5231ae16342d1689b6d01
tudose-bogdan <bogdan-cristian.tudose@my.fmi.unibuc.ro>
2020-12-26 16:29:43 +0200

    merge

--- src/App.js
+++ src/App.js
@@ -1,49 +1,63 @@
 import React, { Component } from 'react'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-
+import themeFile from './util/theme'
+import jwtDecode from 'jwt-decode'
 import './App.css';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+//Redux
+import {Provider} from 'react-redux'
+import store from './redux/store'
 //Components
 import Navbar from './components/Navbar';
+import AuthRoute from './util/AuthRoute'
 //Pages
 import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
-const theme = createMuiTheme({
-  
-  palette: {
-    primary: {
-      main: '#1769aa',
-    },
-    secondary: {
-      main: '#76ff03',
-    },
-  },
-  typography:{
-    useNextVariants: true
-  }
-  
-})
-function App() {
+const theme = createMuiTheme(themeFile);
+
+
+const token = localStorage.FBIdToken;
+let authenticated = false;
+if(token){
+
+    const decodedToken = jwtDecode(token);
+    if(decodedToken.exp * 1000 < Date.now() ){
+      window.location.href='/login'
+      authenticated = false
+    }else{
+      authenticated=true
+    }
+}
+
+
+class App extends Component{
+  render(){
   return (
     <MuiThemeProvider theme={theme}>
-    <div className="App">
+      <Provider store={store}>
+    
     <Router>
     <Navbar/>
       <div className="container">
     <Switch>
       <Route exact path="/" component={home}/>
-      <Route exact path="/login" component={login}/>
-      <Route exact path="/signup" component={signup}/>
+      <AuthRoute exact path="/login" component={login} authenticated={authenticated}/>
+      <AuthRoute exact path="/signup" component={signup}  authenticated={authenticated}/>
 
     </Switch>
+    
       </div>
-    </Router>
-    </div>
+      </Router>
+      </Provider>
+    
+   
+    
     </MuiThemeProvider>
   );
+}
 }
 
 export default App;
---
+++ src/util/theme.js
@@ -0,0 +1,37 @@
+export default {
+    palette: {
+        primary: {
+          main: '#1769aa',
+        },
+        secondary: {
+          main: '#76ff03',
+        },
+      },
+      typography:{
+        useNextVariants: true
+      },
+      form: {
+        textAlign: 'center'
+    },
+    image: {
+        margin: '20px auto 20px auto'
+    },
+    pageTitle: {
+        margin: '10px auto 10px auto'
+    },
+    textField: {
+        margin: '10px auto 10px auto'
+    },
+    button: {
+        marginTop: 20,
+        position: 'relative'
+    },
+    customError: {
+        color:'red',
+        fontSize: '0.8rem',
+        marginTop: 10
+    },
+    progress: {
+        position: 'absolute'
+    }
+}
